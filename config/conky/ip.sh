5b44be882cf7edd9866fa856eca2ee79b6cb097f
Jean Michel Rouly <michel.rouly@gmail.com>
2015-12-24 14:17:23 -0500

    recent update

--- config/conky/ip.sh
+++ config/conky/ip.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-if [ $(cat /sys/class/net/wlan0/operstate) == "up" ];
+if [ $(cat /sys/class/net/wlp2s0/operstate) == "up" ];
 then
-  echo $(ip a show wlan0 | grep "inet " | awk '{ print $2 }')
-fi
-
-if [ $(cat /sys/class/net/eth0/operstate) == "up" ];
+  echo $(ip a show wlp2s0 | grep "inet " | awk '{ print $2 }')
+elif [ $(cat /sys/class/net/enp1s0/operstate) == "up" ];
 then
-  echo $(ip a show eth0 | grep "inet " | awk '{ print $2 }')
+  echo $(ip a show enp1s0 | grep "inet " | awk '{ print $2 }')
+else
+  echo "No IP"
 fi
