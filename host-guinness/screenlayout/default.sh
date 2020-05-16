8f07530728a6d74ad7e236d526f8a02afd098d7d
Jean Michel Rouly <michel.rouly@gmail.com>
2020-05-16 15:16:33 -0400

    guinness i3 configs

--- host-guinness/screenlayout/default.sh
+++ host-guinness/screenlayout/default.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-3 --off --output DVI-I-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-5 --off --output DP-4 --off --output DVI-I-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-1 --off --output DP-0 --off
+xrandr --output DVI-I-0 --off --output DVI-I-1 --off --output HDMI-0 --off --output DP-0 --off --output DP-1 --off --output DP-2 --mode 2560x1440 --pos 0x560 --rotate normal --output DP-3 --off --output DP-4 --mode 2560x1440 --pos 2560x0 --rotate left --output DP-5 --off
