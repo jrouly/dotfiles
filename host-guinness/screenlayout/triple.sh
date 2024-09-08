131ab0805c91fac0f8414080ab3a4483a9007f56
Michel Rouly <michel@rouly.net>
2024-09-08 10:45:30 -0700

    deprecate host-guinness

--- host-guinness/screenlayout/triple.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0 --mode 2560x1440 --pos 4000x0 --rotate left --output DP-1 --off --output HDMI-0 --off --output DP-2 --mode 2560x1440 --pos 0x0 --rotate right --output DP-3 --off --output HDMI-1 --off --output DP-4 --primary --mode 2560x1440 --pos 1440x479 --rotate normal --output DP-5 --off
