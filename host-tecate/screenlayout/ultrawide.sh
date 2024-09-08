518c6badb702633e4968964054d8892326797e10
Michel Rouly <michel@rouly.net>
2024-09-08 10:50:01 -0700

    set ultrawide screenwidth for tecate

---
+++ host-tecate/screenlayout/ultrawide.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+xrandr --output DP-0 --off \
+  --output DP-1 --off \
+  --output HDMI-0 --off \
+  --output DP-2 --off \
+  --output DP-3 --off \
+  --output HDMI-1 --off \
+  --output DP-4 --mode 5120x2160 --pos 0x0 --rotate normal \
+  --output DP-5 --off \
+  --output None-1-1 --off
