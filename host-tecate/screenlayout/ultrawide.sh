57f676e8b797c7fcf8fc16cd56d3b85254007aa2
Michel Rouly <michel@rouly.net>
2025-11-17 17:11:28 -0800

    music and monitor plug reodering

--- host-tecate/screenlayout/ultrawide.sh
+++ host-tecate/screenlayout/ultrawide.sh
@@ -2,8 +2,8 @@
 xrandr --output DP-0 --off \
   --output DP-1 --off \
   --output HDMI-0 --off \
-  --output DP-2 --off \
+  --output DP-2 --mode 5120x2160 --pos 0x0 --rotate normal \
   --output DP-3 --off \
   --output HDMI-1 --off \
-  --output DP-4 --mode 5120x2160 --pos 0x0 --rotate normal \
+  --output DP-4 --off \
   --output DP-5 --off
